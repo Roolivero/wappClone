76b3045bf16756c99b6790bf16fafc9d4126058d
Rocio Jofre Olivero~ <o.rocio@hotmail.com>
2025-04-02 17:42:26 -0300

    feat(backend): :tada: Se inicio el proyecto de kotlin - springboot

---
+++ backend/src/main/kotlin/com/wappClone/WappCloneApplication.kt
@@ -0,0 +1,11 @@
+package com.wappClone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WappCloneApplication
+
+fun main(args: Array<String>) {
+	runApplication<WappCloneApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/wappClone/WappCloneApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wappClone
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WappCloneApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
